e85ec1f3ac3a4c69dbe5e1072f40c204f13efea8
kimmttrung <160985982+kimmttrung@users.noreply.github.com>
2025-03-09 17:11:46 +0700

    render admin

---
+++ src/main/java/com/example/demo/controller/admin/DashBoardController.java
@@ -0,0 +1,14 @@
+package com.example.demo.controller.admin;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class DashBoardController {
+    @GetMapping("/admin")
+    public String getDashBoardPage() {
+        return "admin/dashboard/show";
+    }
+    
+}
